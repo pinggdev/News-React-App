99c7c3393398cb24b9c75767f79e6febf79dbb99
KevinSinagaa <kevinsinaga11.ks@gmail.com>
2021-07-31 00:03:18 +0700

    use service in app component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { useState, useEffect } from "react";
+
 import Navbar from "./components/Navbar";
 import Container from "./components/Container";
 import Loading from "./components/Loading";
 import Error from "./components/Error";
 
+import { getNews } from "./services/getNews";
+
 function App() {
+  useEffect(() => {
+    const fetchTechNews = async () => {
+      const res = await getNews({
+        searchQuery: "microsoft",
+      });
+      console.log(res);
+    };
+    fetchTechNews();
+  }, []);
+
   return (
     <>
       <Navbar />
