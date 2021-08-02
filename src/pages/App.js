6195168e1f26f937100490cae3fd7f79f78c8c7c
KevinSinagaa <kevinsinaga11.ks@gmail.com>
2021-08-02 12:53:49 +0700

    improvement app js

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,23 +1,27 @@
 import { useState, useEffect } from "react";
+import { useParams } from "react-router";
 
-import Navbar from "./components/Navbar";
-import Container from "./components/Container";
-import Loading from "./components/Loading";
-import Error from "./components/Error";
-import NewsList from "./components/NewsList";
+import Navbar from "../components/Navbar";
+import Container from "../components/Container";
+import Loading from "../components/Loading";
+import Error from "../components/Error";
+import NewsList from "../components/NewsList";
 
-import { getNews } from "./services/getNews";
+import { getNews } from "../services/getNews";
 
 function App() {
   const [articles, setArticles] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
 
+  const { id } = useParams();
+  const DEFAULT_SEARCH_QUERY = "microsoft";
+
   useEffect(() => {
     const fetchTechNews = async () => {
       setLoading(true);
       const res = await getNews({
-        searchQuery: "microsoft",
+        searchQuery: id || DEFAULT_SEARCH_QUERY,
       });
 
       if (!res) {
@@ -31,7 +35,7 @@ function App() {
       setArticles(res.articles);
     };
     fetchTechNews();
-  }, []);
+  }, [id]);
 
   return (
     <>
